26ed7398a36e0fe46257def9c0d2afaec4359442
George Czabania <george@czabania.com>
2014-08-22 10:21:22 +1200

    Download tracks

--- pandora/pandora.go
+++ pandora/pandora.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/stayradiated/grooveshark"
+	gs "github.com/stayradiated/grooveshark/responses"
 	"github.com/stayradiated/pandora"
 )
 
@@ -21,43 +23,73 @@ func exists(path string) (bool, error) {
 	return false, err
 }
 
-func download(client *grooveshark.Client, song pandora.Song) {
-	query := song.Name + " " + song.Artist
-	filename := "cache/" + song.Name + " - " + song.Artist + ".mp3"
+func findMatch(song pandora.Song, tracks []gs.Track) gs.Track {
+	return tracks[0]
+}
 
-	if exists, _ := exists(filename); exists {
-		return
-	}
+func sanitize(name string) string {
+	return strings.Replace(name, "/", "_", -1)
+}
 
-	fmt.Println("Seaching", query)
+func download(id int, client *grooveshark.Client, songs <-chan pandora.Song, results chan<- bool) {
+	for song := range songs {
+		query := song.Name + " " + song.Artist
+		filename := "cache/" + sanitize(song.Name+" - "+song.Artist) + ".mp3"
 
-	tracks := client.Search(query)
-	if len(tracks) < 1 {
-		fmt.Println("> Couldn't find a match")
-		return
-	}
+		fmt.Println(id, "##", query)
 
-	track := tracks[0]
+		// don't overwrite files
+		if exists, _ := exists(filename); exists {
+			fmt.Println(id, "> File exists")
+			results <- false
+			continue
+		}
 
-	streamKey := client.GetStreamKey(track.SongId)
+		fmt.Println(id, "> Searching grooveshark for match")
 
-	resp, err := streamKey.Download()
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+		// search grooveshark for the track
+		tracks := client.Search(query)
+		if len(tracks) < 1 {
+			fmt.Println(id, "> Couldn't find a match")
+			results <- false
+			continue
+		}
 
-	output, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer output.Close()
+		// just take the first match
+		// this could be improved to look for higher quality tracks
+		track := findMatch(song, tracks)
 
-	fmt.Println("Downloading", filename)
+		// download the song from grooveshark
+		streamKey, err := client.GetStreamKey(track.SongId)
+		if err != nil {
+			fmt.Println(id, "> Couldn't get a streamKey")
+			results <- false
+			continue
+		}
 
-	_, err = io.Copy(output, resp.Body)
-	if err != nil {
-		panic(err)
+		resp, err := streamKey.Download()
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		// save the file to the cache
+		output, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+		defer output.Close()
+
+		fmt.Println(id, "> Downloading")
+
+		// Copy from the download stream to the file write stream
+		_, err = io.Copy(output, resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		// Let the pool know we have finished
+		results <- true
 	}
 }
 
@@ -66,18 +98,41 @@ func main() {
 	username := os.Args[1]
 	password := os.Args[2]
 
+	fmt.Println("Extracting tracks from Pandora")
+
 	stations, err := pandora.FetchStations(username, password)
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Have stations:", len(stations))
+
 	client := grooveshark.NewClient()
 	client.Connect()
 
+	fmt.Println("Connected to Grooveshark")
+
+	total := 0
+	songs := make(chan pandora.Song, 1000)
+	results := make(chan bool, 1000)
+
+	// start three workers
+	for w := 0; w < 5; w++ {
+		go download(w, client, songs, results)
+	}
+
+	// pass songs into workers
 	for _, station := range stations {
 		for _, song := range station.Songs {
-			download(client, song)
+			total += 1
+			songs <- song
 		}
+	}
+	close(songs)
+
+	// wait for results
+	for i := 0; i < total; i++ {
+		<-results
 	}
 
 }
